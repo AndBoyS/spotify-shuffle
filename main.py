a476264535d026d0eedde7ac9ad4d22f2ad906b6
AndBoyS <andboyxd@gmail.com>
2023-02-25 14:38:51 +0300

    removed extra code

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import os
 
 from tqdm import tqdm
 
@@ -18,10 +17,6 @@ async def main(client, user):
     num_tracks = len(all_tracks)
     shuffler = shuffle.SequentialShuffler(num_tracks)
     shuffle_steps = shuffler.get_shuffle_steps()
-
-    all_tracks_new_order = [None] * len(all_tracks)
-    for old_i, new_i in shuffler.mapping.items():
-        all_tracks_new_order[new_i] = all_tracks[old_i]
 
     for old_index, new_index in tqdm(shuffle_steps, desc='Sending shuffling requests'):
         await user.reorder_tracks(playlist_id, old_index, new_index, 1)
