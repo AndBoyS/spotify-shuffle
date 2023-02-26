c5f5c21d3eed5ff51a0fb612b450c64e0a5c149d
AndBoyS <andboyxd@gmail.com>
2023-02-26 14:30:56 +0300

    postpone recent tracks function

--- src/shuffle.py
+++ src/shuffle.py
@@ -1,24 +1,60 @@
 import random
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Optional, Dict
 
 
 class SequentialShuffler:
     """
     Return the operations (from what positions to move the items to which, without swapping) to shuffle sequentially.
     """
+    # Old index -> new index
+    mapping: Dict[int, int]
 
-    def __init__(self, num_items: int) -> None:
+    def __init__(
+            self,
+            num_items: int,
+            idx_to_postpone: Optional[List[int]] = None,
+            ) -> None:
         self.num_items = num_items
-        old_ids = list(range(num_items))
-        new_ids = list(range(num_items))
-        random.shuffle(new_ids)
+        self.idx_to_postpone = idx_to_postpone or []
+        self.init_shuffle()
 
-        self.old_ids = old_ids
-        self.mapping = dict(zip(old_ids, new_ids))
+    def generate_random_mapping(
+            self,
+            input_idx: List[int],
+            target_idx: List[int],
+            ) -> Dict[int, int]:
+        """
+        Generate the random mapping from the input indexes to the target indexes.
+        """
+        target_idx = target_idx.copy()
+        random.shuffle(target_idx)
+
+        return dict(zip(input_idx, target_idx))
+
+    def init_shuffle(self) -> None:
+        """
+        Initialize the random seed.
+        """
+        old_idx = list(range(self.num_items))
+
+        if self.idx_to_postpone:
+            first_target_idx = old_idx[:-len(self.idx_to_postpone)]  # not postponed
+            postponed_target_idx = old_idx[-len(self.idx_to_postpone):]
+        else:
+            first_target_idx = old_idx
+            postponed_target_idx = []
+
+        mapping = self.generate_random_mapping(old_idx, first_target_idx)
+        mapping.update(self.generate_random_mapping(self.idx_to_postpone, postponed_target_idx))
+
+        self.mapping = mapping
 
     def get_shuffle_steps(self) -> List[Tuple[int, int]]:
-        """Returns the list of moving steps (old_index, new_index) to shuffle sequentially."""
-        temp_order = self.old_ids.copy()
+        """
+        Returns the list of moving steps (old_index, new_index) to shuffle sequentially.
+        Needed for the Spotify API (https://developer.spotify.com/documentation/web-api/reference/playlists/reorder-playlists-tracks/)
+        """
+        temp_order = list(range(self.num_items))
         shuffle_steps = []
 
         # Sort the mapping by new indexes,
---
+++ tests/test_shuffle.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from src import shuffle
+
+
+class TestSequentialShuffler(TestCase):
+    def test_idx_to_postpone(self):
+        num_items = 100
+        idx_to_postpone = list(range(50, 80))
+        shuffler = shuffle.SequentialShuffler(num_items, idx_to_postpone=idx_to_postpone)
+
+        len_idx_to_postpone = len(idx_to_postpone)
+        for old_index, new_index in shuffler.mapping.items():
+            if old_index in idx_to_postpone:
+                assert new_index >= num_items - len_idx_to_postpone
+            else:
+                assert new_index < num_items - len_idx_to_postpone
