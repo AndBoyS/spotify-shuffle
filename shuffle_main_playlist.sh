d60d9c903bd9f6f30aa9a21a557e88fac0042e1f
AndBoyS <andboyxd@gmail.com>
2023-03-19 20:32:51 +0300

    removed osascript

--- shuffle_main_playlist.sh
+++ shuffle_main_playlist.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-osascript -e "display notification \"Spotify playlist shuffle started\" with title \"Job launched\""
 poetry run python main.py 7BMrXBnMPlEiHb0Nd7QcUR
